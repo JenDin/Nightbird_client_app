1ad744a70c41d51a68a73c8e2c9082826a11f730
Jenzi <jennydinh94@gmail.com>
2022-12-16 22:50:06 +0100

    Create dish menu

---
+++ Nightbird_vue_app/src/components/FoodMenu.vue
@@ -0,0 +1,22 @@
+<!-- HTML code -->
+<template>
+  <div class="container max-w-md text-center">
+    <h3>
+      <span class="font-semibold">{{ dish.title }}</span> |
+      <span class="text-right">¥{{ dish.price }}</span>
+    </h3>
+    <p class="text-sm">{{ dish.description }}</p>
+  </div>
+</template>
+
+<!-- JavaScript code -->
+<script>
+export default {
+  props: {
+    dish: Object,
+  },
+};
+</script>
+
+<!-- CSS styles -->
+<style scoped></style>
--- Nightbird_vue_app/src/views/MenuView.vue
+++ Nightbird_vue_app/src/views/MenuView.vue
@@ -1,17 +1,44 @@
 <!-- HTML code -->
 <template>
   <Header darkBorder="black" />
+  <h1 class="text-3xl text-center uppercase font-bold mt-5">Menu</h1>
+  <div class="bg-[#81a58e] p-4 mt-10">
+    <article class="flex justify-center items-center max-w-xl m-auto flex-wrap">
+      <h2 class="text-xl text-center uppercase font-bold">Ikumi's Sushi</h2>
+      <FoodMenu v-for="dish in dishes" :dish="dish" :key="dish.id" />
+    </article>
+  </div>
 </template>
 
 <!-- JavaScript code -->
 <script>
 import Header from "../components/Header.vue";
+import FoodMenu from "../components/FoodMenu.vue";
 export default {
   components: {
     Header,
+    FoodMenu,
   },
   data() {
-    return {};
+    return {
+      dishes: [],
+    };
+  },
+  methods: {
+    async getDishes() {
+      try {
+        const resp = await fetch("http://localhost:3000/api/dishes");
+
+        const data = await resp.json();
+
+        this.dishes = data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.getDishes();
   },
 };
 </script>
